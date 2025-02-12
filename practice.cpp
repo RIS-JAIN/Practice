407690177054d5430e4381de462850a3d76a8680
Rishabh Jain <90439539+RIS-JAIN@users.noreply.github.com>
2025-02-12 12:02:23 +0530

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class test{
+private:
+int code;
+static int count;
+public:
+void setcode(){
+  code=++count;
+}
+static void showcount(){
+  cout<<count;
+}
+};
+int test : count;
+int main(){
+test t1,t2,t3;
+t1.setcode();
+t2.setcode();
+t3.setcode();
+test::showcount();
+}
